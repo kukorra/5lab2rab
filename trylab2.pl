8ccd3cff4e7579c5b7e28ccfec1399bbb1fcb6c6
kukorra <154669365+kukorra@users.noreply.github.com>
2025-06-16 21:39:10 +0500

    Update trylab2.pl

--- trylab2.pl
+++ trylab2.pl
@@ -2,7 +2,6 @@ sum_even([], 0).
 sum_even([H|T], Sum) :-
     (H mod 2 =:= 0 -> sum_even(T, SumRest), Sum is H + SumRest ; sum_even(T, Sum)).
 
-% Стартовая точка программы
 start :-
     write('Введите список (в формате [1,2,3,4]): '),
     read(List),
